13fbc66e801a926cf25676bd6814386aa3781f84
Shadow <117415366+Shadow-Player-One@users.noreply.github.com>
2022-11-04 11:11:09 +1000

    Update streamlit_app.py
    Add in basic menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,10 @@
 import streamlit
 
 streamlit.title('My Parents Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blackberry Oatmeal')
+streamlit.text('Kale Spinach and Rocket Smoothie')
+streamlit.text('Hard Bolied Free Range Egg')
+
+
+
